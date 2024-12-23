4f0b1ee400f60b5a678fc96d01475653f74a0ca2
isma <ismaelmartin302@gmail.com>
2024-12-23 13:53:03 +0100

    `Updated consulting component HTML and added new sections, improved layout and added Angular and NestJS icons`

--- src/app/pages/consulting/consulting.component.ts
+++ src/app/pages/consulting/consulting.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-consulting',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './consulting.component.html',
   styleUrl: './consulting.component.scss'
 })
