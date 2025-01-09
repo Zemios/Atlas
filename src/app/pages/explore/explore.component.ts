b3d512ac7bd35daba9909d3cfcb1f0945f1516cb
isma <ismaelmartin302@gmail.com>
2025-01-09 17:18:31 +0100

    `Added AuthService and implemented authentication check in ExploreComponent`

--- src/app/pages/explore/explore.component.ts
+++ src/app/pages/explore/explore.component.ts
@@ -1,11 +1,12 @@
-import { Component, HostListener, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, HostListener, inject, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 import { PublishComponent } from './publish/publish.component';
 import { PostInterface } from '../../interfaces/post-interface';
 import { PostsService } from '../../services/posts.service';
 import { Observable } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 import { RelativeTimePipe } from '../../pipes/relative-time.pipe';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-explore',
@@ -14,11 +15,27 @@ import { RelativeTimePipe } from '../../pipes/relative-time.pipe';
   templateUrl: './explore.component.html',
   styleUrl: './explore.component.scss',
 })
-export class ExploreComponent {
+export class ExploreComponent implements OnInit {
+  constructor(private router: Router, private authService: AuthService) { }
+  ngOnInit() {
+    this.authService.checkAuth().subscribe({
+      next: (res) => {
+        this.isAuthenticated = res.isAuthenticated
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    })
+  }
+  isAuthenticated: boolean = false;
   private readonly postsSvc = inject(PostsService);
   publishModal = false;
   togglePublishModal() {
-    this.publishModal = !this.publishModal;
+    if (this.isAuthenticated) {
+      this.publishModal = !this.publishModal;
+    } else {
+      this.router.navigate(['/login']);
+    }
   }
   closePublishModal() {
     this.publishModal = false;
