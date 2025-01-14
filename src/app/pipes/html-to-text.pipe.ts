eeb6c2205d735fe9c1c706a379b85c81198246eb
isma <ismaelmartin302@gmail.com>
2025-01-14 16:32:15 +0100

    `Added HTML entity encoding to HtmlToTextPipe transform method`

--- src/app/pipes/html-to-text.pipe.ts
+++ src/app/pipes/html-to-text.pipe.ts
@@ -5,6 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class HtmlToTextPipe implements PipeTransform {
     transform(value: string): string {
+        value = value.replace(/</g, '&lt;').replace(/>/g, '&gt;');
         const doc = new DOMParser().parseFromString(value, 'text/html');
         return doc.body.textContent || "";
     }
