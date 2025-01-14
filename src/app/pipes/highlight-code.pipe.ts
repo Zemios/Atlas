b2dcb8ddd13d51745d5d311ec4c98e3003c29d4a
isma <ismaelmartin302@gmail.com>
2025-01-14 17:00:46 +0100

    `Update HighlightCodePipe to match code blocks with triple backticks instead of 'code' tags`

--- src/app/pipes/highlight-code.pipe.ts
+++ src/app/pipes/highlight-code.pipe.ts
@@ -1,13 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import hljs from 'highlight.js';
 
-
 @Pipe({
   name: 'highlightCode'
 })
 export class HighlightCodePipe implements PipeTransform {
   transform(content: string): string {
-    const regex = /code:([\s\S]*?):code/g;
+    const regex = /``([\s\S]*?)``/g;
     const highlighted = content.replace(regex, (match, codeContent) => {
       const highlightedCode = hljs.highlightAuto(codeContent.trim()).value;
       return `<pre class="bg-gray-100 p-3 rounded"><code>${highlightedCode}</code></pre>`;
