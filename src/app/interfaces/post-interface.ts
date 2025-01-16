31c091d7e0db4aaf02020926670dc150ba50fb0d
isma <ismaelmartin302@gmail.com>
2025-01-16 15:39:25 +0100

    `feat: updated post interface and component to display user profile picture and relative time`

--- src/app/interfaces/post-interface.ts
+++ src/app/interfaces/post-interface.ts
@@ -1,6 +1,5 @@
 export interface PostInterface {
   id: number;
-  user_id: number;
   title: string;
   content: string;
   creation_date: Date;
--- src/app/pages/explore/post/post.component.ts
+++ src/app/pages/explore/post/post.component.ts
@@ -1,3 +1,4 @@
+import { IMAGES_URL } from './../../../app.config';
 import { Observable } from 'rxjs';
 import { Component, inject } from '@angular/core';
 import { PostInterface } from '../../../interfaces/post-interface';
@@ -7,16 +8,18 @@ import { AsyncPipe } from '@angular/common';
 import { NewlineToBrPipe } from '../../../pipes/newline-to-br.pipe';
 import { HighlightCodePipe } from '../../../pipes/highlight-code.pipe';
 import { HtmlToTextPipe } from '../../../pipes/html-to-text.pipe';
+import { RelativeTimePipe } from "../../../pipes/relative-time.pipe";
 
 @Component({
   selector: 'app-post',
-  imports: [AsyncPipe, NewlineToBrPipe, HighlightCodePipe, HtmlToTextPipe],
+  imports: [AsyncPipe, NewlineToBrPipe, HighlightCodePipe, HtmlToTextPipe, RelativeTimePipe],
   templateUrl: './post.component.html',
   styleUrl: './post.component.scss',
 })
 export class PostComponent {
   private readonly postsSvc = inject(PostsService);
   route = inject(ActivatedRoute);
+  IMAGES_URL = IMAGES_URL
   id: string | null = this.route.snapshot.paramMap.get('id');
   post: Observable<PostInterface> = this.postsSvc.get(Number(this.id));
 }
