f82ed1f99fd08c5040ebac2496c61487b23f4e3e
isma <ismaelmartin302@gmail.com>
2025-02-07 19:53:43 +0100

    create course module

---
+++ src/app/pages/course/create-course-module/create-course-module.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-course-module',
+  imports: [],
+  templateUrl: './create-course-module.component.html',
+  styleUrl: './create-course-module.component.scss'
+})
+export class CreateCourseModuleComponent {
+
+}
