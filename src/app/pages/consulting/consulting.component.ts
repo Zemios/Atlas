2c3a479871febf8768a53f0d556a6a0b6dcf61e7
isma <ismaelmartin302@gmail.com>
2025-02-25 16:02:42 +0100

    `feat: updated consulting component with new pricing plans and category selector`

--- src/app/pages/consulting/consulting.component.ts
+++ src/app/pages/consulting/consulting.component.ts
@@ -1,10 +1,21 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 // import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-consulting',
-  // imports: [RouterLink],
+  imports: [NgClass],
   templateUrl: './consulting.component.html',
   styleUrls: ['consulting.component.scss'],
 })
-export class ConsultingComponent { }
+export class ConsultingComponent {
+  activeCategory: string = 'redes';
+
+  setActiveCategory(category: string): void {
+    this.activeCategory = category;
+  }
+
+  isActiveCategory(category: string): boolean {
+    return this.activeCategory === category;
+  }
+}
