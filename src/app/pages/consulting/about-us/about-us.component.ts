7268ed76c6d9672ab88539d0f0501451e858006f
isma <ismaelmartin302@gmail.com>
2025-03-11 14:17:23 +0100

    `Update consulting component with lazy loading for images`

--- src/app/pages/consulting/about-us/about-us.component.ts
+++ src/app/pages/consulting/about-us/about-us.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import AOS from 'aos';
+
 
 @Component({
   selector: 'app-about-us',
@@ -6,4 +9,11 @@ import { Component } from '@angular/core';
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.scss',
 })
-export class AboutUsComponent {}
+export class AboutUsComponent {
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      AOS.init();
+    }
+  }
+}
