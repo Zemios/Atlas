2e211db29565bc8cbd2bc8e539d4e1c27363e650
isma <ismaelmartin302@gmail.com>
2025-04-25 16:46:03 +0200

    `Update plan customization page with improved design and pricing functionality`

--- src/app/pages/consulting/pricing/plan-customization/plan-customization.component.ts
+++ src/app/pages/consulting/pricing/plan-customization/plan-customization.component.ts
@@ -1,41 +1,48 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-plan-customization',
-  imports: [ReactiveFormsModule],
+  imports: [],
   templateUrl: './plan-customization.component.html',
   styleUrl: './plan-customization.component.scss'
 })
 export class PlanCustomizationComponent {
-  form: FormGroup;
-  price = 0;
+  selectedType: string = '';
+  selectedDesign: string = '';
+  selectedExtras: string[] = [];
 
-  constructor(private fb: FormBuilder) {
-    this.form = this.fb.group({
-      type: [''],
-      design: [''],
-      extras: this.fb.group({
-        blog: [false],
-        admin: [false],
-        seo: [false],
-      }),
-    });
+  get price(): number {
+    let total = 0;
 
-    this.form.valueChanges.subscribe(() => this.calculatePrice());
+    const typePrices: any = {
+      landing: 500,
+      corporate: 1000,
+      ecommerce: 2000,
+    };
+
+    const designPrices: any = {
+      custom: 300,
+      default: 0,
+    };
+
+    const extraPrices: any = {
+      blog: 200,
+      admin: 400,
+      seo: 150,
+    };
+
+    if (this.selectedType) total += typePrices[this.selectedType];
+    if (this.selectedDesign) total += designPrices[this.selectedDesign];
+    this.selectedExtras.forEach(extra => total += extraPrices[extra]);
+
+    return total;
   }
 
-  calculatePrice() {
-    let total = 0;
-    const { type, design, extras } = this.form.value;
-
-    total += +type || 0;
-    total += +design || 0;
-
-    if (extras.blog) total += 200;
-    if (extras.admin) total += 400;
-    if (extras.seo) total += 150;
-
-    this.price = total;
+  toggleExtra(extra: string) {
+    if (this.selectedExtras.includes(extra)) {
+      this.selectedExtras = this.selectedExtras.filter(e => e !== extra);
+    } else {
+      this.selectedExtras.push(extra);
+    }
   }
 }
