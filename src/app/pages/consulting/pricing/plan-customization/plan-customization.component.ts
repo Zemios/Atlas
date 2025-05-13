c52f85ac45fd9717b9007951b9f46d1847318f9f
isma <ismaelmartin302@gmail.com>
2025-05-13 12:37:37 +0200

    refactor SVG icons for web consulting images

--- src/app/pages/consulting/pricing/plan-customization/plan-customization.component.ts
+++ src/app/pages/consulting/pricing/plan-customization/plan-customization.component.ts
@@ -32,12 +32,15 @@ type planOption = {
 
 export class PlanCustomizationComponent {
   priceAjustment = 0;
-  selectedWeb: string = 'no-web';
   paymentModeEnum = paymentModeEnum
   paymentMode: paymentModeEnum = paymentModeEnum.ONE_TIME;
 
+  selectedWeb: string = 'no-web';
+  pagesCount: number = 1;
+  webExtrasSelected: string[] = [];
   selectedSocial: string = 'no-social';
-  selectedExtras: string[] = [];
+  postPerWeek: number = 2;
+  socialExtrasSelected: string[] = [];
 
   // Sección Web
   webOptions: planOption[] = [
@@ -45,14 +48,7 @@ export class PlanCustomizationComponent {
     { id: 'web-basic', name: 'Página web básica', price: 300, type: inputType.RADIO, group: 'web' },
     { id: 'tienda', name: 'Tienda online', price: 500, type: inputType.RADIO, group: 'web' },
     { id: 'app-web', name: 'App web con backend', price: 1200, type: inputType.RADIO, group: 'web' },
-    {
-      id: 'web-personalizada', name: 'Página web personalizada', type: inputType.CHECKBOX, group: 'web', options: [
-        { id: 'web-personalizada-1', name: '1 página', price: 0, type: inputType.RADIO, group: 'web-extra' },
-        { id: 'web-personalizada-2', name: '2 páginas', price: 200, type: inputType.RADIO, group: 'web' },
-        { id: 'web-personalizada-3', name: '3 páginas', price: 400, type: inputType.RADIO, group: 'web' },
-        { id: 'web-personalizada-4', name: '4 páginas', price: 600, type: inputType.RADIO, group: 'web' },
-      ]
-    },
+    { id: 'web-personalizada', name: 'Página web personalizada', type: inputType.CHECKBOX, group: 'web' },
   ];
 
   webOptionsExtra: planOption[] = [
@@ -64,20 +60,10 @@ export class PlanCustomizationComponent {
   ]
 
   // Sección Redes Sociales
-  socialOptions: planOption[] = [
-    { id: 'no-social', name: 'Sin gestión de redes sociales', price: 0 },
-    { id: 'social-base', name: 'Red social base', price: 100 },
-    { id: 'red-adicional', name: 'Red adicional', price: 50 },
-    { id: 'publicaciones-3', name: '3 publicaciones semanales', price: 30 },
-    { id: 'publicaciones-4', name: '4 publicaciones semanales', price: 60 },
-    { id: 'publicaciones-5', name: '5 publicaciones semanales', price: 90 },
-    { id: 'historias', name: 'Historias (por red)', price: 20 },
-    { id: 'reels-pack', name: 'Reels/Tiktoks (Pack 4/mes)', price: 100 },
-    { id: 'reels-unitario', name: 'Reels/Tiktoks (unidad)', price: 30 },
+  socialOptionsExtra: planOption[] = [
     { id: 'informe', name: 'Informe de rendimiento', price: 20 },
     { id: 'mensajes', name: 'Atención a mensajes (básico)', price: 40 },
-    { id: 'campanas', name: 'Gestión de campañas publicitarias', price: 50 },
-    { id: 'branding', name: 'Diseño branding redes', price: 60 },
+    { id: 're-branding', name: 'Diseño branding redes', price: 60 },
   ];
 
 
@@ -90,7 +76,7 @@ export class PlanCustomizationComponent {
       if (web && web.price) total += web.price / 10 - 5;
     }
 
-    const social = this.socialOptions.find((s) => s.id === this.selectedSocial);
+    const social = this.socialOptionsExtra.find((s) => s.id === this.selectedSocial);
     if (social && social.price) total += social.price;
 
     return total;
@@ -110,17 +96,10 @@ export class PlanCustomizationComponent {
     return total;
   }
 
-  toggleExtra(extra: string) {
-    this.selectedExtras = this.selectedExtras.includes(extra)
-      ? this.selectedExtras.filter((e) => e !== extra)
-      : [...this.selectedExtras, extra];
-  }
-
   submit() {
     console.log({
       web: this.selectedWeb,
       social: this.selectedSocial,
-      extras: this.selectedExtras,
       total: this.price,
     });
   }
